b1f9745ed9dc75d03bdd8af7d44d51fa1abe0d85
Nathan Toko <toko.albertnathan@hotmail.fr>
2022-05-22 17:37:37 +0200

    Add app color in new class

---
+++ src/models/AppColor.java
@@ -0,0 +1,10 @@
+package src.models;
+
+import java.awt.Color;
+
+public class AppColor {
+    public static final Color PRIMARY = new Color(0x0098E5);
+    public static final Color SECONDARY = new Color(0xEEF2F7);
+    public static final Color TERNARY = new Color(0x26ABA8);
+    public static final Color QUATERNARY = new Color(0xF5C12C);
+}
